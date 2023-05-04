e511505e8c0fc1976b9bf3efb496017e3a854cbe
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-05-04 14:52:57 -0300

    feat: working with nyan

--- src/components/GetParticles.tsx
+++ src/components/GetParticles.tsx
@@ -66,7 +66,7 @@ const GetParticles = () => {
             width: 1,
           },
           collisions: {
-            enable: true,
+            enable: false,
           },
           move: {
             direction: "none",
--- src/components/TopHead.tsx
+++ src/components/TopHead.tsx
@@ -1,29 +1,39 @@
-import './TopHead.css'
+import "./TopHead.scss";
 
 const TopHead = () => {
   return (
-    <div id='headContainer'>
-        <h1>
-        Pedro José Fanelli Ederra
-        </h1>
-        <div id='headText'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus dolor, porro quod suscipit libero explicabo ipsam magnam quia obcaecati doloribus fugiat, dolorem, reiciendis numquam impedit aliquid ad? At, officia autem?
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat, voluptatibus ut. Reiciendis et hic temporibus quam rerum commodi possimus ratione molestias, quos enim praesentium odit molestiae odio accusantium, similique eaque?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos doloremque sequi illum tempora aliquid fugiat excepturi, est corporis doloribus quibusdam maiores libero at placeat? Recusandae quasi saepe repellat natus sunt!
-    </div>
-        <div className='wrapper'>
-            <div className='rainbow'>
-
-            </div>
-            <div className='nyan-cat'>
-                <div className='feet'></div>
-                <div className='tail'></div>
-                <div className='body'></div>
-                <div className='head'></div>
-            </div>
-
+    <>
+      <div id="headContainer">
+        <h1>Pedro José Fanelli Ederra</h1>
+        <div id="headText">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Necessitatibus dolor, porro quod suscipit libero explicabo ipsam
+          magnam quia obcaecati doloribus fugiat, dolorem, reiciendis numquam
+          impedit aliquid ad? At, officia autem? Lorem ipsum dolor, sit amet
+          consectetur adipisicing elit. Quaerat, voluptatibus ut. Reiciendis et
+          hic temporibus quam rerum commodi possimus ratione molestias, quos
+          enim praesentium odit molestiae odio accusantium, similique eaque?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+          doloremque sequi illum tempora aliquid fugiat excepturi, est corporis
+          doloribus quibusdam maiores libero at placeat? Recusandae quasi saepe
+          repellat natus sunt!
         </div>
-    </div>
-  )
-}
+      </div>
+      <div className="wrapper">
+        <div className="rainbow">
+          <span></span>
+        </div>
+        <div className="nyan-cat">
+          <div className="feet"></div>
+          <div className="tail">
+            <span></span>
+          </div>
+          <div className="body"></div>
+          <div className="head"></div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default TopHead
+export default TopHead;
