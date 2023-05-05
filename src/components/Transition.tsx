98af572cc4a800d4942004b1a1b5fcc90a9f6708
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-05-05 12:54:03 -0300

    fix title

--- src/components/Transition.tsx
+++ src/components/Transition.tsx
@@ -5,20 +5,16 @@ import transitionOpacity from "../utils/transitionOpacity";
 const Transition = () => {
   let boxElement: any;
   let title1: any;
-  let title2: any;
 
   useEffect(() => {
     boxElement = document.querySelector(".transition");
     title1 = document.querySelector("#test");
-    title2 = document.querySelector("#test2");
     transitionOpacity(boxElement);
     transitionOpacity(title1, true);
-    transitionOpacity(title2, true);
   }, []);
 
   return (<>
-    <div className="title01"><h2 id="test">First Project</h2></div>
-    <div className="title01"><h2 id="test2">The Movie DataBase</h2></div>
+    <div className="title01"><h2 id="test">The Movie DataBase</h2></div>
     <div className="transition">
       <h2>Buenas</h2>
       <h2>Buenas</h2>
