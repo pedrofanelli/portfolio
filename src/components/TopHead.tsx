45bb86ed2d9b55a8abb80797f2587cf6a1c68a29
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-05-10 15:08:06 -0300

    transition title head text

--- src/components/TopHead.tsx
+++ src/components/TopHead.tsx
@@ -1,10 +1,20 @@
 import "./TopHead.scss";
+import { useEffect } from "react";
+import transitionOpacity from "../utils/transitionOpacity";
 
 const TopHead = () => {
+  let text, title;
+  useEffect(() => {
+    text = document.querySelectorAll("#headText");
+    title = document.querySelectorAll("#headTitle");
+    transitionOpacity(text);
+    transitionOpacity(title);
+  }, []);
+
   return (
     <>
       <div id="headContainer">
-        <h1>Pedro José Fanelli Ederra</h1>
+        <h1 id="headTitle">Pedro José Fanelli Ederra</h1>
         <div id="headText">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit.
           Necessitatibus dolor, porro quod suscipit libero explicabo ipsam
@@ -16,29 +26,27 @@ const TopHead = () => {
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
           doloremque sequi illum tempora aliquid fugiat excepturi, est corporis
           doloribus quibusdam maiores libero at placeat? Recusandae quasi saepe
-          repellat natus sunt!
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-          Necessitatibus dolor, porro quod suscipit libero explicabo ipsam
-          magnam quia obcaecati doloribus fugiat, dolorem, reiciendis numquam
-          impedit aliquid ad? At, officia autem? Lorem ipsum dolor, sit amet
-          consectetur adipisicing elit. Quaerat, voluptatibus ut. Reiciendis et
-          hic temporibus quam rerum commodi possimus ratione molestias, quos
-          enim praesentium odit molestiae odio accusantium, similique eaque?
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
-          doloremque sequi illum tempora aliquid fugiat excepturi, est corporis
-          doloribus quibusdam maiores libero at placeat? Recusandae quasi saepe
-          repellat natus sunt!
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-          Necessitatibus dolor, porro quod suscipit libero explicabo ipsam
-          magnam quia obcaecati doloribus fugiat, dolorem, reiciendis numquam
-          impedit aliquid ad? At, officia autem? Lorem ipsum dolor, sit amet
-          consectetur adipisicing elit. Quaerat, voluptatibus ut. Reiciendis et
-          hic temporibus quam rerum commodi possimus ratione molestias, quos
-          enim praesentium odit molestiae odio accusantium, similique eaque?
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
-          doloremque sequi illum tempora aliquid fugiat excepturi, est corporis
-          doloribus quibusdam maiores libero at placeat? Recusandae quasi saepe
-          repellat natus sunt!
+          repellat natus sunt! Lorem ipsum dolor sit amet, consectetur
+          adipisicing elit. Necessitatibus dolor, porro quod suscipit libero
+          explicabo ipsam magnam quia obcaecati doloribus fugiat, dolorem,
+          reiciendis numquam impedit aliquid ad? At, officia autem? Lorem ipsum
+          dolor, sit amet consectetur adipisicing elit. Quaerat, voluptatibus
+          ut. Reiciendis et hic temporibus quam rerum commodi possimus ratione
+          molestias, quos enim praesentium odit molestiae odio accusantium,
+          similique eaque? Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Dignissimos doloremque sequi illum tempora aliquid fugiat
+          excepturi, est corporis doloribus quibusdam maiores libero at placeat?
+          Recusandae quasi saepe repellat natus sunt! Lorem ipsum dolor sit
+          amet, consectetur adipisicing elit. Necessitatibus dolor, porro quod
+          suscipit libero explicabo ipsam magnam quia obcaecati doloribus
+          fugiat, dolorem, reiciendis numquam impedit aliquid ad? At, officia
+          autem? Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          Quaerat, voluptatibus ut. Reiciendis et hic temporibus quam rerum
+          commodi possimus ratione molestias, quos enim praesentium odit
+          molestiae odio accusantium, similique eaque? Lorem ipsum dolor sit
+          amet consectetur adipisicing elit. Dignissimos doloremque sequi illum
+          tempora aliquid fugiat excepturi, est corporis doloribus quibusdam
+          maiores libero at placeat? Recusandae quasi saepe repellat natus sunt!
           Lorem ipsum dolor sit amet, consectetur adipisicing elit.
           Necessitatibus dolor, porro quod suscipit libero explicabo ipsam
           magnam quia obcaecati doloribus fugiat, dolorem, reiciendis numquam
@@ -51,6 +59,9 @@ const TopHead = () => {
           doloribus quibusdam maiores libero at placeat? Recusandae quasi saepe
           repellat natus sunt!
         </div>
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+          doloremque sequi illum tempora aliquid fugiat excepturi, est corporis
+          doloribus quibusdam maiores libero at placeat?</div>
       </div>
     </>
   );
--- src/components/Transition.tsx
+++ src/components/Transition.tsx
@@ -4,8 +4,7 @@ import transitionOpacity from "../utils/transitionOpacity";
 import Card from "../commons/Card";
 
 const Transition = () => {
-  let container: any;
-  let title1: any;
+  let container, title1;
   let varios = [1, 2, 3];
 
   useEffect(() => {
