c253c6bf020542cb0cf67e6ff77d7fe6160f03c1
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-05-12 12:28:40 -0300

    download

--- src/components/TopHead.tsx
+++ src/components/TopHead.tsx
@@ -12,13 +12,29 @@ const TopHead = () => {
     transitionOpacity(title);
   }, []);
 
+  function handleDownload() {
+    const filePath = "src/assets/PedroFanelliResume.pdf";
+    const link = document.createElement("a");
+    link.href = filePath;
+    link.download = "Pedro Fanelli Resume.pdf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
+
   return (
     <>
       <div id="headContainer">
         <h1 id="headTitle">Pedro José Fanelli Ederra</h1>
         <div id="headText">
-          <div> FULL STACK WEB DEVELOPER y <br /> Abogado <br /><Button variant="outlined">Download</Button> </div>
-          
+          <div>
+            
+            FULL STACK WEB DEVELOPER y <br /> Abogado <br />
+            <Button variant="contained" onClick={handleDownload} >
+              Download Resume
+            </Button>
+          </div>
+
           <img src="src/assets/myPic.jpg" alt="me" />
         </div>
       </div>
