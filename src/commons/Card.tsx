16e64d0b4e74e6ea03e458cf6d9c23ebe2d90c5f
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-05-15 11:22:26 -0300

    working with card

--- src/commons/Card.tsx
+++ src/commons/Card.tsx
@@ -1,8 +1,10 @@
 import { FC } from "react";
 
 interface Props {
-  id: number,
-  name: string
+  id: number;
+  name: string;
+  desc: string;
+  tech: string;
 }
 
 const Card: FC<Props> = (prop) => {
@@ -12,8 +14,10 @@ const Card: FC<Props> = (prop) => {
         <h2 id="title">{prop.name}</h2>
       </div>
       <div className="card">
-        <h2>Text</h2>
-        <h2>Text</h2>
+        <div style={{display:"flex", flexDirection:"column", width:"800px", textAlign:"center"}}>
+          <h2>Technologies <br /> {prop.tech}</h2>
+          <h2>Description <br /> {prop.desc}</h2>
+        </div>
         <img src="src/assets/cat.jpg" alt="Cat pic" />
       </div>
     </div>
