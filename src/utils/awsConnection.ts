c9f2e22c13121dbd88426c0843b9c79666e2a689
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-05-17 12:43:29 -0300

    cred

--- src/utils/awsConnection.ts
+++ src/utils/awsConnection.ts
@@ -3,14 +3,14 @@ import AWS from "aws-sdk";
 const s3download = function () {
   
   AWS.config.update({
-    accessKeyId: "AKIAT45X27YLXTBOGUML",
-    secretAccessKey: "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",
+    accessKeyId: "",
+    secretAccessKey: "",
     region: "us-east-1",
   });
 
   const s3 = new AWS.S3();
 
-  const params = {Bucket: "pf-portfoliovideos", Key: "video2.mov"}
+  const params = {Bucket: "", Key: ""}
 
   return new Promise((resolve, reject) => {
     s3.getObject(params, function (err, data) {
