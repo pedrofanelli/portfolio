52b3159eb509bec74313d8e026f310a355ddd4b4
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-05-19 09:34:09 -0300

    contact form

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,37 +1,38 @@
-import "./Contact.scss"
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
+import "./Contact.scss";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
 const Contact = () => {
   return (
     <div id="contactContainer">
       <div id="contactTitle">
         <h1>Contact</h1>
       </div>
-      <div>
       <Box
-      component="form"
-      sx={{
-        '& .MuiTextField-root': { m: 1, width: '25ch' },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <div>
+        component="form"
+        sx={{
+          //"& .MuiTextField-root": { m: 1, width: "75ch" },
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+        noValidate
+        autoComplete="off"
+      >
         <TextField
           required
           id="outlined-required"
           label="Required"
           defaultValue="Hello World"
+          sx={{m:1, width:"600px"}}
         />
         <TextField
-          disabled
           id="outlined-disabled"
           label="Disabled"
           defaultValue="Hello World"
+          sx={{m:1, width:"600px"}}
         />
-        </div>
-        </Box>
-      </div>
+      </Box>
     </div>
   );
 };
