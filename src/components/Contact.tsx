6267fdbeecbc2ef97e95c681328a5780dfce6530
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-05-19 10:41:07 -0300

    contact form

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@mui/material";
 import "./Contact.scss";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
@@ -15,6 +16,27 @@ const Contact = () => {
           justifyContent: "center",
           alignItems: "center",
           flexDirection: "column",
+          zIndex: 5,
+          "& label.Mui-focused": {
+            color: "whitesmoke",
+          },
+          "& .MuiInput-underline:after": {
+            borderBottomColor: "yellow",
+          },
+          "& .MuiOutlinedInput-root": {
+            "& fieldset": {
+              borderColor: "whitesmoke",
+            },
+            "&:hover fieldset": {
+              borderColor: "whitesmoke",
+            },
+            "&.Mui-focused fieldset": {
+              borderColor: "#f03a47",
+            },
+          },
+          "& label": {
+            color: "whitesmoke",
+          },
         }}
         noValidate
         autoComplete="off"
@@ -22,16 +44,47 @@ const Contact = () => {
         <TextField
           required
           id="outlined-required"
-          label="Required"
-          defaultValue="Hello World"
-          sx={{m:1, width:"600px"}}
+          label="Your email"
+          //defaultValue="Hello World"
+          sx={{ m: 1, width: "600px" }}
+          inputProps={{
+            style: {
+              color: "white",
+              boxShadow:
+                "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+            },
+          }}
         />
         <TextField
-          id="outlined-disabled"
-          label="Disabled"
-          defaultValue="Hello World"
-          sx={{m:1, width:"600px"}}
+          required
+          id="outlined-multiline-flexible"
+          label="Text"
+          multiline
+          rows={8}
+          sx={{
+            m: 1,
+            width: "600px",
+            boxShadow:
+              "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+          }}
+          inputProps={{ style: { color: "white" } }}
         />
+        <Button
+          type="submit"
+          size="large"
+          variant="contained"
+          //onClick={(e) => handleDownload(e)}
+          sx={{
+            textTransform: "none",
+            m: 2,
+            backgroundColor: "#f03a47",
+            ":hover": { backgroundColor: "#f03a47", opacity: 0.9 },
+            boxShadow:
+              "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+          }}
+        >
+          SEND
+        </Button>
       </Box>
     </div>
   );
