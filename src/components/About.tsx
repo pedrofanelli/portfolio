46eda02548e203c601ab33ada36b360b344f16e4
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-05-20 09:45:52 -0300

    about end

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,18 +1,21 @@
 import Footer from "./Footer";
+import "./About.scss";
 
 const About = () => {
   return (
     <>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, maxime? Molestias, consequuntur?</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, maxime? Molestias, consequuntur?</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, maxime? Molestias, consequuntur?</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, maxime? Molestias, consequuntur?</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, maxime? Molestias, consequuntur?</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, maxime? Molestias, consequuntur?</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, maxime? Molestias, consequuntur?</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, maxime? Molestias, consequuntur?</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, maxime? Molestias, consequuntur?</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, maxime? Molestias, consequuntur?</div>
+      <div id="aboutCont">
+        <div>As a Lawyer I spent years studying and training in the legal world. Throughout my career, I have developed skills in research, analysis and problem solving, which have enabled me to tackle complex challenges. <br />
+
+However, all my life I had a great affinity with computers. Curiosity led me to take the CS50x course as an introductory course to web programming, and it was a one-way trip, I fell in love with code. That's why I decided to change course and embark on a new adventure as a Full Stack web developer. <br />
+
+Through my training and experience in law, I have developed skills that help me excel as a developer. For example, my ability to analyze and synthesize complex information allows me to quickly understand new technologies and apply them effectively in development projects. <br />
+
+As a developer throughout the Plataforma 5 Bootcamp, I worked on several exciting projects (such as the internship for Globant) where I was able to collaborate with the team to create unique and high-quality applications. The exercise of the SCRUM methodology was fundamental throughout the development of the projects, turning out to be an invaluable experience. <br />
+
+I am excited and motivated about this new career in technology and am looking forward to continuing to learn and grow as a web developer. I am always open to new opportunities and look forward to working on innovative and challenging projects in the future 💫</div>
+        <img src="src/assets/myPic.jpg" alt="me" />
+      </div>
       <Footer />
     </>
   );
