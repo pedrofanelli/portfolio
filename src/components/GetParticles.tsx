64ca73b13c8d260755b6df7e2a2a4e1652f85452
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-05-20 17:11:57 -0300

    cleaning logs

--- src/components/GetParticles.tsx
+++ src/components/GetParticles.tsx
@@ -1,7 +1,7 @@
 import { useCallback } from "react";
 import Particles from "react-particles";
 import { loadFull } from "tsparticles";
-import type { Container, Engine } from "tsparticles-engine";
+import type { Engine } from "tsparticles-engine";
 
 const GetParticles = () => {
   const particlesInit = useCallback(async (engine: Engine) => {
